2130d21d9490b62c36baf823adb2ec37101a0ab2
verenka <verenka@gmail.com>
2015-09-01 20:34:08 +0200

    saving to local storage works

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
  * Created by verenka on 30/08/15.
  */
 
+// load the measurements so far from local storage
+if (localStorage.getItem("measurements") != null) {
+    measurements = JSON.parse(localStorage.getItem("measurements"));
+} else {
+    measurements = [];
+}
+
 // when the page is loaded the following needs to happen
 // - show form, hide day overview
 // - date/time form field filled with current date/time
@@ -35,15 +42,14 @@ $(document).ready(function(){
 
     $("#corr").change(function () {
         calcBolus();
-        alert($("#timestamp").val());
     });
 
 });
 
 // save needs to do the following:
-// - hide form
-// - show day overview
-// - save data into offline storage
+// - hide form - ok
+// - show day overview - ok
+// - save data into offline storage - ok
 // - save data into database
 // - get data from storage / database
 // - display data for that day
@@ -66,7 +72,22 @@ function cancelMeasurements() {
 
 
 function saveToLocal() {
-    var timestamp = $("#timestamp").val();
+    var day = $("#timestamp").val().substr(0,10);
+    var time =$("#timestamp").val().substr(11,15);
+    var bloodsugar = $("#bloodsugar").val();
+    var be = $("#be").val();
+    var iebe = $("#iebe").val();
+    var prandial = calcPrandial();
+    var corr = $("#corr").val();
+    var bolus = calcBolus();
+    var basal = $("#basal").val();
+    var comment = $("#comm").val();
+
+    var entry = {"day": day, "time":time, "bloodsugar": bloodsugar, "be": be, "iebe": iebe, "prandial": prandial,
+                 "corr": corr, "bolus": bolus, "basal": basal, "comment": comment};
+    measurements[measurements.length] = entry;
+
+    localStorage.setItem("measurements", JSON.stringify(measurements));
 }
 
 
@@ -94,6 +115,7 @@ function calcPrandial() {
     } else {
         $("#prandial").html(" 0");
     }
+    return prandial
 }
 
 function calcBolus() {
@@ -105,4 +127,5 @@ function calcBolus() {
     } else {
         $("#bolus").html("0");
     }
+    return total;
 }
