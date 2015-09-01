b241a3c9bbf3d3ab6d97ac9cc984a2c13f2f2660
verenka <verenka@gmail.com>
2015-09-01 19:53:23 +0200

    finished calculated fields (bolus & prandial)

--- js/script.js
+++ js/script.js
@@ -18,28 +18,27 @@ $(document).ready(function(){
         startView: '0'
     }).val();
 
-    $("#save").click(saveMeasurements());
-    $("#cancel").click(cancelMeasurements());
+    $("#save").click(function(){
+        saveMeasurements();
+    });
+    $("#cancel").click(function() {
+        cancelMeasurements();
+    });
+
+    $("#be").change(function () {
+        calcPrandial();
+    });
+
+    $("#iebe").change(function () {
+        calcPrandial();
+    });
+
+    $("#corr").change(function () {
+        calcBolus();
+        alert($("#timestamp").val());
+    });
 
 });
-
-function saveMeasurements() {
-    showOverview();
-}
-
-function cancelMeasurements() {
-    showOverview()
-}
-
-function showForm() {
-    $("#dayview").hide();
-    $("#measure").show();
-}
-
-function showOverview() {
-    $("#dayview").show();
-    $("#measure").hide();
-}
 
 // save needs to do the following:
 // - hide form
@@ -49,8 +48,61 @@ function showOverview() {
 // - get data from storage / database
 // - display data for that day
 
+
+function saveMeasurements() {
+    showOverview();
+    saveToLocal();
+}
+
 // cancel needs to do:
 // - hide form
 // - empty form fields
 // - show day overview
 // - display data for that day
+
+function cancelMeasurements() {
+    showOverview()
+}
+
+
+function saveToLocal() {
+    var timestamp = $("#timestamp").val();
+}
+
+
+function showForm() {
+    $("#dayview").hide();
+    $("#measure").show();
+    $("#menu_measure").attr('class','active');
+    $("#menu_dayview").removeAttr('class');
+}
+
+function showOverview() {
+    $("#dayview").show();
+    $("#measure").hide();
+    $("#menu_dayview").attr('class','active');
+    $("#menu_measure").removeAttr('class');
+
+}
+
+function calcPrandial() {
+    var be = $("#be").val();
+    var iebe = $("#iebe").val();
+    if (be > 0 && iebe > 0) {
+        var prandial = Math.round(be * iebe);
+        $("#prandial").html(prandial);
+    } else {
+        $("#prandial").html(" 0");
+    }
+}
+
+function calcBolus() {
+    var prandial = $("#prandial").html()-0; // minus zero added, so js will treat it as numbers
+    var corr = $("#corr").val()-0;
+    if (prandial > 0 && corr > 0) {
+        var total = prandial + corr;
+        $("#bolus").html(total);
+    } else {
+        $("#bolus").html("0");
+    }
+}
